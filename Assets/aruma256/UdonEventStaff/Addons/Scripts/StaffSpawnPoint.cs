488d8375eb9402e4df11757be29d6240fc021f43
aruma256 <22253623+aruma256@users.noreply.github.com>
2024-01-02 22:59:56 +0900

    StaffSpawnPoint スタッフ専用スポーンポイント設定スクリプトを追加

---
+++ Assets/aruma256/UdonEventStaff/Addons/Scripts/StaffSpawnPoint.cs
@@ -0,0 +1,40 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class StaffSpawnPoint : UdonSharpBehaviour
+{
+    [SerializeField] UdonEventStaff udonEventStaff;
+
+    void Start()
+    {
+        if (udonEventStaff == null) {
+            Debug.Log("ワールドエラー: UdonEventStaff がリンクされていません。");
+        }
+    }
+
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        OnPlayerSpawn(player);
+    }
+
+    public override void OnPlayerRespawn(VRCPlayerApi player)
+    {
+        OnPlayerSpawn(player);
+    }
+
+    private void OnPlayerSpawn(VRCPlayerApi player)
+    {
+        if (udonEventStaff == null) return;
+        if (!player.isLocal) return;
+        if (!udonEventStaff.AmIStaff()) return;
+        player.TeleportTo(
+            transform.position,
+            transform.rotation,
+            VRC_SceneDescriptor.SpawnOrientation.Default,
+            false
+        );
+    }
+}
