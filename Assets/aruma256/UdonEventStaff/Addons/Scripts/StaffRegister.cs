561a070a83612b8263a7d7a7b39594349bae52b8
aruma256 <22253623+aruma256@users.noreply.github.com>
2024-01-02 17:13:40 +0900

    StaffRegister スタッフ昇格ボタン を実装した

---
+++ Assets/aruma256/UdonEventStaff/Addons/Scripts/StaffRegister.cs
@@ -0,0 +1,34 @@
+﻿using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class StaffRegister : UdonSharpBehaviour
+{
+    [SerializeField] UdonEventStaff udonEventStaff;
+    [SerializeField] Text miniText;
+    [SerializeField] Button button;
+
+    void Start()
+    {
+        if (udonEventStaff == null) {
+            miniText.text = "ワールドエラー: UdonEventStaff がリンクされていません。";
+        }
+    }
+
+    public void OnClick()
+    {
+        if (udonEventStaff == null) return;
+        if (udonEventStaff.AmIStaff()) {
+            miniText.text = "すでにスタッフです。";
+            button.interactable = false;
+            return;
+        }
+        miniText.text = "";
+        udonEventStaff.BecomeStaff();
+        miniText.text = "スタッフになりました。";
+        button.interactable = false;
+    }
+}
