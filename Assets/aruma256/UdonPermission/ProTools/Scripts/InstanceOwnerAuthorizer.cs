319f384de0085991e111661eeff12f7b043d6743
aruma256 <22253623+aruma256@users.noreply.github.com>
2024-01-06 23:10:01 +0900

    InstanceOwnerAuthorizer を実装した

---
+++ Assets/aruma256/UdonPermission/ProTools/Scripts/InstanceOwnerAuthorizer.cs
@@ -0,0 +1,55 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class InstanceOwnerAuthorizer : UdonSharpBehaviour
+{
+    [SerializeField] UdonEventStaff udonEventStaff;
+
+    [UdonSynced(UdonSyncMode.None), FieldChangeCallback(nameof(instanceOwnerName))] private string _instanceOwnerName = "";
+    private string instanceOwnerName
+    {
+        get => _instanceOwnerName;
+        set
+        {
+            _instanceOwnerName = value;
+            OnInstanceOwnerNameSet();
+        }
+    }
+
+    void Start()
+    {
+        if (udonEventStaff == null) {
+            Debug.Log("[StaffRegister_Manual] UdonEventStaff がリンクされていません。");
+        }
+    }
+
+    public override void OnPlayerJoined(VRCPlayerApi player)
+    {
+        if (udonEventStaff == null) return;
+        if (!player.isLocal) return;
+        if (player.playerId != 1) return;
+        //
+        BecomeOwner();
+        instanceOwnerName = Networking.LocalPlayer.displayName;
+        RequestSerialization();
+    }
+
+    private void OnInstanceOwnerNameSet()
+    {
+        if (udonEventStaff == null) return;
+        if (instanceOwnerName != Networking.LocalPlayer.displayName) return;
+        udonEventStaff.BecomeStaff();
+    }
+
+    // Utility Functions
+
+    private bool IsOwner() => Networking.IsOwner(gameObject);
+    private void BecomeOwner()
+    {
+        if (IsOwner()) return;
+        Networking.SetOwner(Networking.LocalPlayer, gameObject);
+    }
+}
