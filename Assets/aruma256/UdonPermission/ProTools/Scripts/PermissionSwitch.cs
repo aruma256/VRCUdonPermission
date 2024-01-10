bbf3f3920c339c934b82e9723927d73d116b8d53
aruma256 <22253623+aruma256@users.noreply.github.com>
2024-01-10 23:56:21 +0900

    PermissionSwitchを追加

---
+++ Assets/aruma256/UdonPermission/ProTools/Scripts/PermissionSwitch.cs
@@ -0,0 +1,33 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class PermissionSwitch : UdonSharpBehaviour
+{
+    [Header("UdonPermissionへのリンク")]
+    [SerializeField] UdonPermission udonPermission;
+    [Header("Useで権限を付与する")]
+    [SerializeField] bool givePermissionOnUse = true;
+    [Header("Useで権限を剥奪する")]
+    [SerializeField] bool revokePermissionOnUse = true;
+
+    void Start()
+    {
+        if (udonPermission == null) {
+            Debug.Log("ワールドエラー: UdonPermission がリンクされていません。");
+        }
+    }
+
+    public override void Interact()
+    {
+        if (udonPermission == null) return;
+        bool hasPermission = udonPermission.HasPermission();
+        if (hasPermission && revokePermissionOnUse) {
+            udonPermission.RevokePermission();
+        } else if (!hasPermission && givePermissionOnUse) {
+            udonPermission.GivePermission();
+        }
+    }
+}
