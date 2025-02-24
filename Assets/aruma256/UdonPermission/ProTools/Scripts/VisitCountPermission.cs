4874551c9e7a305fac0915a187e00ed30dc70d6b
aruma256 <22253623+aruma256@users.noreply.github.com>
2025-02-24 15:59:19 +0900

    VisitCountPermission を実装

---
+++ Assets/aruma256/UdonPermission/ProTools/Scripts/VisitCountPermission.cs
@@ -0,0 +1,45 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Aruma256.UdonPermission
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class VisitCountPermission : UdonSharpBehaviour
+    {
+        [Header("UdonPermissionへのリンク")]
+        [SerializeField] private UdonPermission udonPermission;
+        [Header("◯回以上の訪問で権限を付与")]
+        [SerializeField] private int requiredVisitCount = 3;
+
+        [UdonSynced(UdonSyncMode.None)] private int _visitCount = 0;
+
+        void Start()
+        {
+            if (udonPermission == null)
+            {
+                Debug.Log("[VisitCountPermission] UdonPermission がリンクされていません。");
+                return;
+            }
+        }
+
+        public override void OnPlayerRestored(VRCPlayerApi player)
+        {
+            if (!Networking.IsOwner(gameObject)) return;
+
+            _visitCount++;
+            RequestSerialization();
+
+            if (_visitCount >= requiredVisitCount)
+            {
+                udonPermission.GivePermission();
+            }
+        }
+
+        public int GetVisitCount()
+        {
+            return _visitCount;
+        }
+    }
+}
