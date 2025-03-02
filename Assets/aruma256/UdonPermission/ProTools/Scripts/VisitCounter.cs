cf1eb85f44d7e99c76bec4ba606ee3c3bdc37ec4
aruma256 <22253623+aruma256@users.noreply.github.com>
2025-03-02 21:07:30 +0900

    VisitCountPermissionをVisitCounterに分離するためにコピーした

---
+++ Assets/aruma256/UdonPermission/ProTools/Scripts/VisitCounter.cs
@@ -0,0 +1,75 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using TMPro;
+using VRC.SDK3.Persistence;
+
+namespace Aruma256.UdonPermission
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class VisitCounter : UdonSharpBehaviour
+    {
+        [Header("UdonPermissionへのリンク")]
+        [SerializeField] private UdonPermission udonPermission;
+        [Header("◯回以上の訪問で権限を付与")]
+        [SerializeField] private ulong requiredVisitCount = 3;
+        [Header("同日の再訪問をカウントしない")]
+        [SerializeField] private bool ignoreSameDayVisits = false;
+        [Header("訪問回数表示用のText（オプション）")]
+        [SerializeField] private TextMeshProUGUI visitCountText;
+        [Header("PlayerDataのキー設定（任意）")]
+        [SerializeField] private string visitCountKey = "UdonPermission_VisitCount";
+        [SerializeField] private string lastVisitDateKey = "UdonPermission_LastVisitDate";
+        
+        private ulong _visitCount = 0;
+        private string _lastVisitDateTime = "";
+
+        void Start()
+        {
+            if (udonPermission == null)
+            {
+                Debug.Log("[VisitCountPermission] UdonPermission がリンクされていません。");
+                return;
+            }
+        }
+
+        public override void OnPlayerRestored(VRCPlayerApi player)
+        {
+            // 「他者がロード完了したよ」は無視
+            if (!player.isLocal) return;
+
+            // PlayerDataから訪問回数と最終訪問日時を取得
+            _visitCount = PlayerData.GetULong(player, visitCountKey);
+            _lastVisitDateTime = PlayerData.GetString(player, lastVisitDateKey) ?? "0000-00-00 00:00:00";
+
+            string now = System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            string today = now.Substring(0, 10); // "yyyy-MM-dd" part
+            bool shouldCount = !ignoreSameDayVisits || _lastVisitDateTime.CompareTo(today) < 0;
+
+            if (shouldCount)
+            {
+                _visitCount++;
+                // PlayerDataに訪問回数を保存
+                PlayerData.SetULong(visitCountKey, _visitCount);
+            }
+
+            // 訪問回数が条件を満たしているかチェック
+            if (_visitCount >= requiredVisitCount)
+            {
+                udonPermission.GivePermission();
+            }
+
+            // PlayerDataに最終訪問日時を保存
+            PlayerData.SetString(lastVisitDateKey, now);
+            
+            UpdateVisitCountDisplay();
+        }
+
+        private void UpdateVisitCountDisplay()
+        {
+            if (visitCountText == null) return;
+            visitCountText.text = _visitCount.ToString();
+        }
+    }
+}
